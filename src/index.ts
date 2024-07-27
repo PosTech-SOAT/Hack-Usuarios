0f3a5d532b729f6da4875bc2fba956ecac6948c4
Dedyanna Cacau <dedyannacacau@gmail.com>
2024-07-26 21:49:34 -0300

    feat: api users(doctors and patients) ajustes 2

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,11 @@ const app = express();
 app.use(express.json());
 app.use('/api', router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`🔥 Server listening at http://localhost:${PORT}/api`);
 	console.log(`Database Info:
         Host: ${process.env.DB_HOST},
-        User: ${process.env.DB_USER},
-        Password: ${process.env.DB_PASS},
         Database Name: ${process.env.DB_NAME}`);
 });
